ea32bd8e3604e76c8d6be24d44ea3d04050017d0
IanZweig <44142825+IanZweig@users.noreply.github.com>
2025-11-04 16:42:16 -0700

    Update submission.cs

--- submission.cs
+++ submission.cs
@@ -17,6 +17,7 @@ namespace ConsoleApp1
     public static string xmlURL = "https://ianzweig.github.io/cse455_a4/Hotels.xml";
     public static string xmlErrorURL = "https://ianzweig.github.io/cse455_a4/HotelsErrors.xml";
     public static string xsdURL = "https://ianzweig.github.io/cse455_a4/Hotels.xsd";
+    public string output = "";
     public static void Main(string[] args)
     {
       string result = Verification(xmlURL, xsdURL);
@@ -29,7 +30,7 @@ namespace ConsoleApp1
     // Q2.1
     public static string Verification(string xmlUrl, string xsdUrl)
     {
-      string output = "";
+      output = "";
       //return "No Error" if XML is valid. Otherwise, return the desired exception message.
       // Create the XmlSchemaSet class.
       XmlSchemaSet sc = new XmlSchemaSet();
@@ -43,19 +44,24 @@ namespace ConsoleApp1
       settings.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
       XmlReader reader = XmlReader.Create(xmlUrl, settings);
       while (reader.Read());
-      
+      if(output.equals(""))
+      {
+        output = "No errors are found";
+      }
       return output;
     }
     public static string Xml2Json(string xmlUrl)
     {
-      // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. 
-      string jsonText = "";
+      XmlDocument doc = new XmlDocumunet();
+      doc.Load(xmlUrl);
+      // The returned jsonText needs to be de-serializable by Newtonsoft.Json package.
+      string jsonText = JsonConvert.SerializeXmlNode(doc);
       return jsonText;
     }
 
     private static void ValidationCallBack(object sender, ValidationEventArgs e)
     {
-      Console.WriteLine("Validation Error: {0}", e.Message);
+      output += e.Message;
     }
   }
 }
