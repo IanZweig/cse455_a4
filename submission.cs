115eb1591ba3205e6ae658f62acaef75dccf23ab
IanZweig <44142825+IanZweig@users.noreply.github.com>
2025-11-04 15:56:53 -0700

    Update submission.cs

--- submission.cs
+++ submission.cs
@@ -14,9 +14,9 @@ namespace ConsoleApp1
 {
   public class Program
   {
-    public static string xmlURL = "https://ianzweig.github.io/cse_a4/Hotels.xml";
-    public static string xmlErrorURL = "https://ianzweig.github.io/cse_a4/HotelsErrors.xml";
-    public static string xsdURL = "https://ianzweig.github.io/cse_a4/Hotels.xsd";
+    public static string xmlURL = "https://ianzweig.github.io/cse455_a4/Hotels.xml";
+    public static string xmlErrorURL = "https://ianzweig.github.io/cse455_a4/HotelsErrors.xml";
+    public static string xsdURL = "https://ianzweig.github.io/cse455_a4/Hotels.xsd";
     public static void Main(string[] args)
     {
       string result = Verification(xmlURL, xsdURL);
